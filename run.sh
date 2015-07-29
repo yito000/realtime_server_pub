7ec8f84caf9a1bf6d36c6e5b9e8353b77f6830fa
yito <ito@Apple-no-MacBook-Pro.local>
2015-07-29 17:39:28 +0900

    スクリプト修正

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ p=`pwd`
 export DYLD_FALLBACK_LIBRARY_PATH=$p/libs/lib:$DYLD_FALLBACK_LIBRARY_PATH
 
 if test $# -gt 1; then
-  $p/realtime_server $1 $2
+  $p/build/realtime_server $1 $2
 else
   echo $p
 
-  $p/realtime_server
+  $p/build/realtime_server
 fi
 
