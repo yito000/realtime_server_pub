2502cfc7a3a9d355834b903d6ef5e1b60c327c96
yito <ito@Apple-no-MacBook-Pro.local>
2015-08-28 14:32:17 +0900

    微修正

--- src/app/flatbuffers/glue_fb.cc
+++ src/app/flatbuffers/glue_fb.cc
@@ -1,8 +1,8 @@
 #include "glue_fb.h"
 
-#include <stdio.h>
+#include "log/logger.h"
 
 void test(int n, const std::string& str, long actor_key)
 {
-    printf("n=%d, str=%s\n", n, str.c_str());
+    Logger::log("n=%d, str=%s", n, str.c_str());
 }
