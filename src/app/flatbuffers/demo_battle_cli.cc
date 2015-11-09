e199396d9ced002fafa5a3c0db27af30275a048f
yito <ito@MacBook-Pro.local>
2015-11-09 15:27:44 +0900

    ログ追加

--- src/app/flatbuffers/demo_battle_cli.cc
+++ src/app/flatbuffers/demo_battle_cli.cc
@@ -1,12 +1,14 @@
 #include "demo_battle_cli.h"
 
+#include "log/logger.h"
+
 namespace DemoBattle {
 
 void input_command(int player_id, 
     const flatbuffers::Vector<const CommandDetail *>* list,
     long actor_key)
 {
-    // TODO
+    Logger::log("input player id=%d, command size=%d", player_id, list->size());
 }
 
 };
