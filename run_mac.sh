50ea56de51db513da96874a35e2e630fc21537a3
yito <ito@MacBook-Pro.local>
2015-12-09 20:25:14 +0900

    起動スクリプトをmacとLinuxで分けた

--- run_mac.sh
+++ run_mac.sh
@@ -1,10 +1,13 @@
 p=`pwd`
 
+ulimit -n 2048
 export DYLD_FALLBACK_LIBRARY_PATH=$p/libs/lib:$DYLD_FALLBACK_LIBRARY_PATH
 
 if test $# -gt 1; then
   $p/build/realtime_server $1 $2
 else
+#  echo $p
+
   $p/build/realtime_server
 fi
 
